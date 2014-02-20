fad5987c216fc8ba01f9fa56819bf90f5b634365
ajay <ajay786singh@gmail.com>
2014-02-20 08:21:58 +0530

    slider place in between content

--- wordpress/wp-content/themes/hwtc/page_about.php
+++ wordpress/wp-content/themes/hwtc/page_about.php
@@ -3,41 +3,7 @@
 ?>
 <?php get_header();?>
 <?php if(have_posts()):while(have_posts()):the_post(); ?>
-<?php
-$args = array(
-        'post_type' => 'slide',
-        'posts_per_page' => 10
-	);	
-	
-	// The Query
-	$the_query = new WP_Query( $args );
-	
-	// Check if the Query returns any posts
-	if ( $the_query->have_posts() ) {
-	
-	// Start the Slider ?>
-		<ul class="slider">
-			
-			<?php while ( $the_query->have_posts() ) : $the_query->the_post(); 
-			$url = wp_get_attachment_url( get_post_thumbnail_id($post->ID, 'banner') );
-			?>
-			
-				<li class="slide-container" style="background-image:url(<?php echo $url;?>);">
-					<img src="<?php echo $url;?>" alt="<?php the_title();?>" />
-                    <div class="bannertext">
-                        <div class="bannertext-content">
-                        	<div class="banner-title-big"><?php the_title();?></div>
-                        </div>     
-                   </div>
-				</li>
-				
-			<?php endwhile; ?>
-	
-		</ul><!-- .slider-->
-		<?php 
-	// Reset Post Data
-	wp_reset_postdata();
-	} else { 
+		<?php  
 	$img_src = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full', false, ''); 
 	if($img_src!='') { 	
 		$img=$img_src[0];
@@ -59,7 +25,6 @@ $args = array(
         </div>
 </div>
 
-<?php } ?>
 
 <div class="container">
     <div class="columns-1 content">
