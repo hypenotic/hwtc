5602e9cdd9979c0c27c154dcf3215e4d622d86d2
Lionel Mann <lionelmann@gmail.com>
2015-09-13 21:09:26 -0400

    newsletter edits

--- wordpress/wp-content/themes/hwtc/category-hospitalitytrainingtimes.php
+++ wordpress/wp-content/themes/hwtc/category-hospitalitytrainingtimes.php
@@ -9,12 +9,20 @@ $img= get_bloginfo('template_url').'/images/banner-newsletter.jpg';
 		<img src="<?php echo $img;?>" />
         <div class="bannertext">
             <div class="bannertext-content">
-                <div class="banner-title-big"><?php single_cat_title();?></div>
+                <div class="banner-title-big">Hospitality Training Times</div>
             </div>
         </div>
 </div>
 <div class="full-width content">
 	<div class="container">
+	<p style="width: 70%;">
+	The Hospitality Workers Training Centre (HWTC) is your training centre.
+	We work in partnership with Unite Here, Local 75 to make sure you have access to skills and training
+	that meets your needs. We offer classes on a range of topics from English and computer classes to hands on skills like banquet and restaurant serving and Smart Serve. Many of the classes take place right in your properties.All our small classes are led by professionals who are there to help you succeed.
+	Please visit our <a href="http://hospitalitytrainingcentre.com">website</a> to learn about our upcoming training programs:
+	and “Like” us on <a href="https://www.facebook.com/HospitalityTrainingCentre">Facebook</a> to stay up to date.
+</p>
+
 		<?php 
 		if(have_posts()):
 			echo "<ul class='blog-lists'>";
