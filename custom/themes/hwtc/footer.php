f71dd0ac925b4dad403b96adef1d6e4291469707
Emily Dela Cruz <delacruz.emily@gmail.com>
2018-07-11 14:35:38 -0400

    Update footer info

--- custom/themes/hwtc/footer.php
+++ custom/themes/hwtc/footer.php
@@ -20,17 +20,24 @@
         </div>
         
         <div class="columns-2">
-            <h4>Contact</h4>
+            <h4 style="margin-bottom: 0;">Contact</h4>
     
             <ul class="social-media">
 	            <li><a id="facebook" href="https://www.facebook.com/pages/Hospitality-Workers-Training-Centre/152350521594605" target="_blank">Facebook</a></li>
                 <li><a id="twitter" href="https://twitter.com/Hospitalitywrks" target="_blank">Twitter</a></li>
             </ul>
-            <p><a href="mailto:training@hospitalitytrainingcentre.com">training@hospitalitytrainingcentre.com</a></p>
-            <p><a href="tel:6479309518">647 930 9518</a></p>
+
+            <p style="margin-top: 0px;"><a href="tel:6479309518">647 930 9518</a></p>
+            
+            <h5 style="margin-bottom: 0; font-size: 20px;">Mailing Address</h5>
 			<p>Hospitality Workers Training Centre<br />
             60 Richmond Street East<br />
             Toronto, Ontario, Canada M5C 1N8</p>
+            
+            <h5 style="margin-bottom: 0; font-size: 20px;"><a href="http://hospitalitytrainingcentre.com/find-skilled-talent/new-to-hospitality/">Information Sessions</a></h5>
+            <p>489 Queen Street East Suite 301<br />
+            Toronto, Ontario, Canada M5A 1V1</p>
+            <p><a href="mailto:training@hospitalitytrainingcentre.com">training@hospitalitytrainingcentre.com</a></p>
         </div>
     </div>
     <div class="footer-copyright">
